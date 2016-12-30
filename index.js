e4de773ff10f97b58d397b88533973f1ac83373e
Mariusz Bąk <mariusz.bak@xsolve.pl>
2016-12-30 12:30:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,40 @@ var ini = require('ini'),
     fs  = require('fs'),
     merge = require('merge');
 
-var config = merge.recursive(
-    parseConfig('main'),
-    parseConfig('env')
-);
-
 /**
  * Parse /config/name.ini
  *
+ * @param {string} path Path
  * @param {string} name Filename
  * @returns {array} Parameters
  */
-function parseConfig(name) {
-    name = process.cwd() + '/config/' + name + '.ini';
+function parseConfig(path, name) {
+    name = path + '/config/' + name + '.ini';
     return ini.parse(fs.readFileSync(name, 'utf-8'));
 }
 
-/**
- * Apply one section of the config on itself
- * For instance, apply test section for test environment
- * 
- * @param {string} section Section of ini-config
- */
-config.apply = function(section) {
-    
-    if (!this.hasOwnProperty(section)) {
-        throw new Error('Section "' + section + '" not found in config');
-    }
-    
-    merge.recursive(this, this[section]);
-};
+module.exports = function (path) {
 
-module.exports = config;
+    var config = merge.recursive(
+        parseConfig(path, 'main'),
+        parseConfig(path, 'env')
+    );
+
+    /**
+     * Apply one section of the config on itself
+     * For instance, apply test section for test environment
+     * 
+     * @param {string} section Section of ini-config
+     */
+    config.apply = function(section) {
+
+        if (!this.hasOwnProperty(section)) {
+            throw new Error('Section "' + section + '" not found in config');
+        }
+
+        merge.recursive(this, this[section]);
+    };
+
+    return config;
+    
+};
